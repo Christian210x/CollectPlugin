06a7360260c464e5502d2e16e2ca94a153805acd
Wantsome909 <134110923+Christian210x@users.noreply.github.com>
2023-11-04 18:58:31 -0600

    Add files via upload
    just a test for uploading purposes

--- src/main/java/me/wantsome/plugins/collect/CollectPlugin.java
+++ src/main/java/me/wantsome/plugins/collect/CollectPlugin.java
@@ -1,6 +1,7 @@
 package me.wantsome.plugins.collect;
 
 import me.wantsome.plugins.collect.configs.MessageConfig;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CollectPlugin extends JavaPlugin {
@@ -23,7 +24,11 @@ public final class CollectPlugin extends JavaPlugin {
 
 
     private void initConfigs(){
+        long start = System.currentTimeMillis();
+
         this.messageConfig = new MessageConfig(this);
+
+        Bukkit.getServer().getLogger().info("Successfully initialized Configs! [" + getTimeDifference(start, System.currentTimeMillis()) + "]");
     }
 
     public String getTimeDifference(long start, long end) {
